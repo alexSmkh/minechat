51087f4684e282be0297174594a112e6b11f192a
Alexander Samokhvalov <alexshikari9333@gmail.com>
2023-02-07 01:20:13 +0300

    feat: save the message history to a file

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import asyncio
 import sys
+from datetime import datetime
+
+import aiofiles
+
+
+async def write_chat_history(message: str) -> None:
+    async with aiofiles.open('chat.txt', mode='a') as file:
+        time = datetime.now().strftime('%d.%m.%y %H:%M:%S')
+        await file.write(f'[{time}] {message}')
 
 
 async def connect_to_chat(host: str, port: str) -> None:
@@ -10,8 +19,11 @@ async def connect_to_chat(host: str, port: str) -> None:
     while True:
         try:
             async with asyncio.timeout(5):
-                messages = await reader.readline()
-            print(messages.decode())
+                message = await reader.readline()
+
+            decoded_message = message.decode()
+            await write_chat_history(decoded_message)
+            print(decoded_message)
 
             error_counter = 0
         except TimeoutError:

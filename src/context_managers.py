dc11e356e4a0a05802da71ba1d98d7a49331e93b
Alexander Samokhvalov <alexshikari9333@gmail.com>
2023-03-02 19:17:50 +0300

    fix: considered the case if connection fails to establish

--- src/context_managers.py
+++ src/context_managers.py
@@ -4,9 +4,11 @@ from contextlib import asynccontextmanager
 
 @asynccontextmanager
 async def connection_manager(host, port):
+    writer = None
     try:
         reader, writer = await asyncio.open_connection(host, port)
         yield reader, writer
     finally:
-        writer.close()
-        await writer.wait_closed()
+        if writer is not None:
+            writer.close()
+            await writer.wait_closed()
